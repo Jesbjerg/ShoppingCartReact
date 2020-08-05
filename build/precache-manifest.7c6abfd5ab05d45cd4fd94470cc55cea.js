5b0f6833024ad1375ddbd417983685753b2b3001
Jesbjerg <jesperenemark@hotmail.com>
2020-08-05 11:18:33 +0200

    allowed prod build into github

---
+++ build/precache-manifest.7c6abfd5ab05d45cd4fd94470cc55cea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860bb421bf4c5939e136082dbbc5fe81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f8f59d4aea09017f3e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d54820f89fe946a57520",
+    "url": "/static/js/2.3656563e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3656563e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f8f59d4aea09017f3e",
+    "url": "/static/js/main.51efe205.chunk.js"
+  },
+  {
+    "revision": "534a3a78d30bd5c0bdc2",
+    "url": "/static/js/runtime-main.795e4ebc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
