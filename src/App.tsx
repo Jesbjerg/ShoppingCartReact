36727a5f1c7de3ee153b1fc1c39a33df5a53a472
Jesbjerg <jesperenemark@hotmail.com>
2020-08-05 13:15:59 +0200

    changes was made to test pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Changes was made and expecting no deployment in pipeline
         </p>
         <a
           className="App-link"
